2538008cf9990cb77ba5ca44fe1a1320f0c0f072
Stanislav Zaleski <sigmaray@gmail.com>
2020-04-28 13:38:26 +0300

    Don't do rake db:drop

--- test-cypress.sh
+++ test-cypress.sh
@@ -1,2 +1,2 @@
-docker-compose run -e "RAILS_ENV=test" web-test rake db:drop db:create db:migrate
+docker-compose run -e "RAILS_ENV=test" web-test rake db:create db:migrate
 docker-compose run -e "CYPRESS_BASE_URL=http://web-test:5017" web-test yarn cypress run --project ./spec
--- test-rspec.sh
+++ test-rspec.sh
@@ -1,2 +1,2 @@
-docker-compose run -e "RAILS_ENV=test" web rake db:drop db:create db:migrate
+docker-compose run -e "RAILS_ENV=test" web rake db:create db:migrate
 docker-compose run -e "RAILS_ENV=test" web rspec
